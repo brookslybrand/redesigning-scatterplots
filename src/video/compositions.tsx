6410da07eaafd89f0dae914bb4eb932a2508ee70
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2021-03-09 13:52:20 -0600

    Setup redesigning-scatterplots file

--- src/video/compositions.tsx
+++ src/video/compositions.tsx
@@ -1,19 +1,15 @@
 import { Composition } from 'remotion'
-/** @jsxImportSource @emotion/react */
 import { GlobalStyles } from 'twin.macro'
+import RedesigningScatterPlots from './redesigning-scatterplots'
 
 export default function Compositions() {
   return (
     <>
       <GlobalStyles />
       <Composition
-        id="into-my-own"
-        component={() => (
-          <div tw="w-full h-full bg-white text-center">
-            <h1 tw="mt-16 text-6xl">Hi</h1>
-          </div>
-        )}
-        durationInFrames={17 * (30 + 45)} // This could probably be more data driven, but whatever
+        id="redesigning-scatterplots"
+        component={RedesigningScatterPlots}
+        durationInFrames={150}
         fps={30}
         width={1920}
         height={1080}
---
+++ src/video/redesigning-scatterplots.tsx
@@ -0,0 +1,10 @@
+/** @jsxImportSource @emotion/react */
+import 'twin.macro'
+
+export default function RedesigningScatterPlots() {
+  return (
+    <div tw="w-full h-full bg-white text-center">
+      <h1 tw="mt-16 text-6xl">Hi</h1>
+    </div>
+  )
+}
