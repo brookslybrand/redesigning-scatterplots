55f866efe90fa55b7fdcc8a7f8928c1daebd9442
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2021-03-10 08:50:01 -0600

    Add plot placeholder and add fade out for paragraph

--- src/video/compositions.tsx
+++ src/video/compositions.tsx
@@ -12,7 +12,7 @@ export default function Compositions() {
       <Composition
         id="redesigning-scatterplots"
         component={RedesigningScatterPlots}
-        durationInFrames={100}
+        durationInFrames={200}
         fps={30}
         width={1920}
         height={1080}
--- src/video/redesigning-scatterplots.tsx
+++ src/video/redesigning-scatterplots.tsx
@@ -1,46 +1,117 @@
 /** @jsxImportSource @emotion/react */
 import tw, { css } from 'twin.macro'
-import { useCurrentFrame, interpolate, Sequence, Easing } from 'remotion'
+import {
+  useCurrentFrame,
+  interpolate,
+  Sequence,
+  Easing,
+  useVideoConfig,
+} from 'remotion'
+
+const widthHeightRatio = 36 / 51 // ratio of the width to the height as measured in the book
+const plotWidth = 550
 
 export default function RedesigningScatterPlots() {
   return (
-    <div tw="w-full h-full bg-white flex flex-col items-center">
-      <h1 tw="mt-16 bl-text-6xl mb-8">Hi, this is Poppins</h1>
+    <div tw="w-full h-full bg-gray-yellow-200 flex flex-col items-center">
+      <PlotPlaceholder />
+
       <div
         css={[
           css`
-            width: 36rem;
+            width: ${plotWidth}px;
           `,
         ]}
       >
-        <Paragraph>{text}</Paragraph>
+        <Sequence
+          from={0}
+          durationInFrames={200}
+          name="paragraph 1"
+          layout="none"
+        >
+          <Paragraph>{text}</Paragraph>
+        </Sequence>
       </div>
       {/* <MyVideo /> */}
     </div>
   )
 }
 
+function PlotPlaceholder() {
+  return (
+    <div
+      css={[
+        tw`mt-16 mb-12 bg-transparent border-2 border-gray-red-400`,
+        css`
+          width: ${plotWidth}px;
+          height: ${plotWidth * widthHeightRatio}px;
+        `,
+      ]}
+    />
+  )
+}
+
 const text =
-  'A large share of ink on a graphic should present data-information, the ink changing as the data change. *Data-ink* is the non--erasable core of a graphic, the non-redundant ink arranged in response to variation in the numbers represented. Then,'
+  'A large share of ink on a graphic should present data-information, the ink changing as the data change. *Data-ink* is the non-erasable core of a graphic, the non-redundant ink arranged in response to variation in the numbers represented. Then,'
 
 const ease = Easing.bezier(0.25, 0.1, 0.25, 1.0)
 
 type ParagraphProps = {
-  inDuration?: number
+  transformDuration?: number
+  fadeDuration?: number
   children: React.ReactNode
 }
-function Paragraph({ inDuration = 40, children }: ParagraphProps) {
+function Paragraph({
+  transformDuration = 40,
+  fadeDuration = transformDuration + 10,
+  children,
+}: ParagraphProps) {
   const frame = useCurrentFrame()
-  const y = interpolate(frame, [0, inDuration], [25, 0], {
-    easing: ease,
-    extrapolateLeft: 'clamp',
-    extrapolateRight: 'clamp',
-  })
-  const opacity = interpolate(frame, [0, inDuration + 10], [0, 1], {
-    easing: ease,
-    extrapolateLeft: 'clamp',
-    extrapolateRight: 'clamp',
-  })
+  const { durationInFrames } = useVideoConfig() // paragraph exists as long as the duration
+  const transformOutStart = durationInFrames - transformDuration
+  const fadeOutStart = durationInFrames - fadeDuration
+  if (transformOutStart < transformDuration || fadeOutStart < fadeDuration) {
+    throw new Error(`
+    Not enough time allotted to transform or fade out.
+    transform out start: ${transformOutStart}, fade out start: ${fadeOutStart}
+    `)
+  }
+
+  const y =
+    frame < transformOutStart
+      ? interpolate(frame, [0, transformDuration], [25, 0], {
+          easing: ease,
+          extrapolateLeft: 'clamp',
+          extrapolateRight: 'clamp',
+        })
+      : interpolate(
+          frame,
+          [transformOutStart, transformOutStart + transformDuration],
+          [0, 25],
+          {
+            easing: ease,
+            extrapolateLeft: 'clamp',
+            extrapolateRight: 'clamp',
+          }
+        )
+
+  const opacity =
+    frame < fadeOutStart
+      ? interpolate(frame, [0, fadeDuration], [0, 1], {
+          easing: ease,
+          extrapolateLeft: 'clamp',
+          extrapolateRight: 'clamp',
+        })
+      : interpolate(
+          frame,
+          [fadeOutStart, fadeOutStart + fadeDuration],
+          [1, 0],
+          {
+            easing: ease,
+            extrapolateLeft: 'clamp',
+            extrapolateRight: 'clamp',
+          }
+        )
 
   return (
     <p
